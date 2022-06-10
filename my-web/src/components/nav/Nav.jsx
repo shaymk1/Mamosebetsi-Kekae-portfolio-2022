46990a4f041e640814326dd321a46da32973d377
Mamosebetsi Kekae <mkekae@gmail.com>
2022-06-10 16:10:12 -0700

    added nav and home components as well as the react icons library and uninstalled framer animation

---
+++ my-web/src/components/nav/Nav.jsx
@@ -0,0 +1,59 @@
+import './nav.css'
+
+
+import React from 'react';
+
+
+import { Link } from 'react-scroll';
+const Nav = () => {
+	return (
+		<div className="n-wrapper" id="Navbar">
+			{/* left */}
+			<div className="n-left">
+				<div className="n-name">Shay</div>
+			</div>
+			{/* right */}
+			<div className="n-right">
+				<div className="n-list">
+					<ul style={{ listStyleType: 'none' }}>
+						<li>
+							<Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link to="services" spy={true} smooth={true}>
+								Serivces
+							</Link>
+						</li>
+						<li>
+							<Link to="works" spy={true} smooth={true}>
+								Experience
+							</Link>
+						</li>
+						<li>
+							<Link to="portfolio" spy={true} smooth={true}>
+								Protfolio
+							</Link>
+						</li>
+						<li>
+							<Link to="testimonial" spy={true} smooth={true}>
+								Testimonial
+							</Link>
+						</li>
+						<li>
+							<Link to="testimonial" spy={true} smooth={true}>
+							Contact
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Nav;
+
+
+// <button className="button n-button">Contact</button>;
